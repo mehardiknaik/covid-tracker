7850df633ac716b4549196830115bdaa16a327f1
Hardik Naik <hardiknaik7444@gmail.com>
2022-01-09 01:24:03 +0530

    header colour chnage on scroll

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,10 +1,21 @@
 import { Typography } from "@mui/material"
+import { useState } from "react";
 import logo from '../images/logo.png';
 import styles from "./Header.module.css"
 
 const Header = () => {
+    const [color, setcolor] = useState('transparent')
+    const changeNavbarColor = () =>{
+        if(window.scrollY >= 54){
+            setcolor('#e6e6e6');
+        }
+        else{
+            setcolor('transparent');
+        }
+     };
+     window.addEventListener('scroll', changeNavbarColor);
     return (
-        <div className={styles.Container}>
+        <div className={styles.Container} style={{background: color}}>
             <img className={styles.image} src={logo} width={35} height={35} alt=""/>
            <Typography variant="h5" >Covid Tracker </Typography> 
            <Typography color="textSecondary">(India)</Typography>

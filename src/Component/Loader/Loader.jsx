5818e3f80aa92c7399c5fcfd8d1a91c5478a057d
Hardik Naik <hardiknaik7444@gmail.com>
2022-01-09 22:58:45 +0530

    sponner

--- src/Component/Loader/Loader.jsx
+++ src/Component/Loader/Loader.jsx
@@ -1,12 +1,16 @@
-import styles from "./Loader.module.css"
-import logo from '../images/logo.png';
+import styles from "./Loader.module.css";
 
 const Loader = () => {
-    return (
-        <div className={styles.container}>
-            <img className={styles.image} src={logo} width={80} height={80} alt=""/>
+  return (
+    <div className={styles.container}>
+        <div className={styles.leoborder1}>
+          <div className={styles.leocore1}></div>
         </div>
-    )
-}
+        <div className={styles.leoborder2}>
+          <div className={styles.leocore2}></div>
+        </div>
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;

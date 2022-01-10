a62f425f76c9a2c875af5741f461b1fd24849e56
Hardik Naik <hardiknaik7444@gmail.com>
2022-01-10 11:21:54 +0530

    loader

--- src/Component/Loader/Loader.jsx
+++ src/Component/Loader/Loader.jsx
@@ -3,12 +3,7 @@ import styles from "./Loader.module.css";
 const Loader = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.leoborder1}>
-          <div className={styles.leocore1}></div>
-        </div>
-        <div className={styles.leoborder2}>
-          <div className={styles.leocore2}></div>
-        </div>
+      <span className={styles.loader}></span>
     </div>
   );
 };

b32c7eb166f130cfa6ac218754152da4a334f98b
Hardik Naik <hardiknaik7444@gmail.com>
2022-01-19 17:15:00 +0530

    Babout changes

--- src/Component/Contactme/Contactme.jsx
+++ src/Component/Contactme/Contactme.jsx
@@ -1,43 +1,80 @@
+import { makeStyles } from "@mui/styles";
+import { Card, CardContent, Container, Grid } from "@mui/material";
+import styles from "./Contact.module.css";
 
-import { makeStyles } from '@mui/styles';
-
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   container: (props) => ({
-    position: 'relative',
-    margin: 'auto',
+    position: "relative",
+    margin: "auto",
     background: props.primary,
-    height: 'fit-content',
-    width: 'fit-content',
-    padding: '3px 16px',
-    borderBottomRightRadius: '6px',
-    borderTopRightRadius:' 6px',
-    borderBottomLeftRadius: '12px',
-    borderTopLeftRadius: '12px',
-    display: 'flex',
-    alignItems: 'center',
-    transition: '0.7s',
-    cursor:'pointer',
-    '&::before':{
+    height: "fit-content",
+    width: "fit-content",
+    padding: "3px 16px",
+    borderBottomRightRadius: "6px",
+    borderTopRightRadius: " 6px",
+    borderBottomLeftRadius: "12px",
+    borderTopLeftRadius: "12px",
+    display: "flex",
+    flexDirection: "column",
+    transition: "0.7s",
+    cursor: "pointer",
+    "&::before": {
       content: '""',
       background: props.secondary,
-      transform: 'translateX(-27px)',
-      position: 'absolute',
-      height: '11px',
-      width: '11px',
-      borderRadius: '50px',
-      border: '3px solid white',
+      transform: "translateX(-27px)",
+      position: "absolute",
+      height: "11px",
+      width: "11px",
+      borderRadius: "50px",
+      border: "3px solid white",
     },
-    '&:hover':{
-      transform:'scale(1.1)',
-    }
+    "&:hover": {
+      transform: "scale(2)",
+      zIndex: 2,
+      "& $textcontainer": {
+        transform: "translateY(20px)",
+      },
+    },
   }),
+  textcontainer: {
+    overflow: "hidden",
+  },
+  secondtext: {
+    transform: "translateY(23px)",
+    transition: "0.7s",
+  },
 }));
 
-const Contactme = (props) => {
+const Contactme = () => {
+  return (
+    <>
+      <h2 className={styles.header}>Contact Me</h2>
+      <Container>
+        <Grid item component={Card} xs={12} md={12} className={styles.card}>
+          <CardContent>
+            <div className={styles.CardContainer}>
+              <Link primary="#e6e6ff" secondary="#0000cb" text="Facebook" />
+              <Link primary="#fde6ff" secondary="#bb00cb" text="Instagram" />
+              <Link primary="#ffe6e6" secondary="#e70000" text="Mail" />
+            </div>
+          </CardContent>
+        </Grid>
+      </Container>
+    </>
+  );
+};
+
+const Link = (props) => {
   const classes = useStyles(props);
 
-  return <div className={classes.container}>{props.text}</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.textcontainer}>
+        <div>{props.text}</div>
+        {/* <div className={classes.secondtext}>test</div> */}
+      </div>
+    </div>
+  );
 };
 
 export default Contactme;
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,55 +1,6 @@
-import { Card, CardContent, Container, Grid } from "@mui/material";
 import Contactme from "../Component/Contactme/Contactme";
 const About = () => {
-  return (
-    <>
-      <h2 style={{ textAlign: "center" }}>About</h2>
-      <Container>
-        <Grid
-          item
-          component={Card}
-          xs={12}
-          md={12}
-          style={{
-            borderBottom: `10px solid #f00`,
-            height: "calc(100vh - 134px)",
-            display: "flex",
-            justifyContent: "center",
-            textAlign: "center",
-            alignItems: "center",
-          }}
-        >
-          <CardContent>
-            <div>
-              this is test word written to test about section of this app
-            </div>
-            <h4>Contact Me</h4>
-            <div
-              style={{
-                display: "flex",
-                marginTop: "20px",
-                flexWrap: "wrap",
-                gap: "20px",
-              }}
-            >
-              <Contactme
-                primary="#e6e6ff"
-                secondary="#0000cb"
-                text="Facebook"
-              />
-              <Contactme
-                primary="#fde6ff"
-                secondary="#bb00cb"
-                text="Instagram"
-              />
-              <Contactme primary="#ffe6e6" secondary="#e70000" text="Mail" />
-              {/* <Contactme /> */}
-            </div>
-          </CardContent>
-        </Grid>
-      </Container>
-    </>
-  );
+  return <Contactme />;
 };
 
 export default About;
